17a4dfb486efefcc4cc79d8987d9b16aef456896
Anastasiia <anastasiia.perih@gmail.com>
2023-12-16 23:00:48 -0500

    Overview page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Products from "pages/Products";
 import Customers from 'pages/Customers'
 import Transactions from 'pages/Transactions'
 import Geography from 'pages/Geography'
+import Overview from "pages/Overview";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -29,6 +30,7 @@ function App() {
               <Route path="/customers" element={<Customers />} />
               <Route path="/transactions" element={<Transactions />} />
               <Route path="/geography" element={<Geography />} />
+              <Route path="/overview" element={<Overview />} />
             </Route>
           </Routes>
         </ThemeProvider>
