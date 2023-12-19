ebc2d69ddcbca2e1a2325497b7fa9091fdf7ddab
Anastasiia <anastasiia.perih@gmail.com>
2023-12-18 21:23:44 -0500

    admin page

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Overview from "pages/Overview";
 import Daily from "pages/Daily";
 import Monthly from "pages/Monthly";
 import Breakdown from "pages/Breakdown";
+import Admin from "pages/Admin";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -37,6 +38,7 @@ function App() {
               <Route path="/daily" element={<Daily />} />
               <Route path="/monthly" element={<Monthly />} />
               <Route path="/breakdown" element={<Breakdown />} />
+              <Route path="/admin" element={<Admin />} />
             </Route>
           </Routes>
         </ThemeProvider>
--- src/state/api.js
+++ src/state/api.js
@@ -3,7 +3,15 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
   reducerPath: "adminApi",
-  tagTypes: ["User", "Products", "Customers", "Transactions", 'Geography', 'Sales'],
+  tagTypes: [
+    "User",
+    "Products",
+    "Customers",
+    "Transactions",
+    "Geography",
+    "Sales",
+    "Admins",
+  ],
   endpoints: (build) => ({
     getUser: build.query({
       query: (id) => `general/user/${id}`,
@@ -23,16 +31,20 @@ export const api = createApi({
         method: "GET",
         params: { page, pageSize, sort, search },
       }),
-      providesTags: ["Transactions"]
+      providesTags: ["Transactions"],
     }),
     getGeography: build.query({
-      query: () => 'client/geography',
-      providesTags: ['Geography']
+      query: () => "client/geography",
+      providesTags: ["Geography"],
     }),
     getSales: build.query({
-      query: () => 'sales/sales',
-      providesTags: ['Sales']
-    })
+      query: () => "sales/sales",
+      providesTags: ["Sales"],
+    }),
+    getAdmins: build.query({
+      query: () => "management/admin",
+      providesTags: ['Admins'],
+    }),
   }),
 });
 
@@ -42,5 +54,6 @@ export const {
   useGetCustomersQuery,
   useGetTransactionsQuery,
   useGetGeographyQuery,
-  useGetSalesQuery
+  useGetSalesQuery,
+  useGetAdminsQuery
 } = api;
